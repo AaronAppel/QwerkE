ed58e910f815a15bdea8d1dc2907f8a36f59f4c7
JaminGamer <alaa@rogers.com>
2018-02-22 21:51:12 -0500

    Forgot to commit the solution so I remade it. I started adding files back in and I wanted to commit the solution for now. -Aaron

--- QwerkE/Cleanup.bat
+++ QwerkE/Cleanup.bat
@@ -1,5 +1,21 @@
-delete /Q /S .vs
-rmdir .vs
+del /Q /F /S /AH .vs\*
+del /Q /F /S x64
+rmdir /Q /S .vs
+rmdir /Q /S x64
 
-delete /Q /S Debug
-rmdir Debug
+del /Q /F /S /AH Debug
+rmdir /Q /S Debug
+
+cd Test_Game1
+del /Q /F /S Debug
+del /Q /F /S x64
+rmdir /Q /S Debug
+rmdir /Q /S x64
+cd ../
+
+cd QwerkE_Win32
+del /Q /F /S Debug
+del /Q /F /S x64
+rmdir /Q /S Debug
+rmdir /Q /S x64
+cd ../
