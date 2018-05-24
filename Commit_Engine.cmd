b8d71998ab737d60ee24c7b77ee2114952d81e86
aaronappel <aaron.appel@outlook.com>
2018-05-24 14:56:11 -0400

    Improved auto commit to wrap apostrophes and add my name. -Aaron

--- Commit_Engine.cmd
+++ Commit_Engine.cmd
@@ -8,7 +8,7 @@ pause
 rem commit local changes to local repo
 set /p message=Commit message "..."?
 git add .
-git commit -m %message%
+git commit -m "%message%. -Aaron"
 
 rem push local commit up to master
 git push
