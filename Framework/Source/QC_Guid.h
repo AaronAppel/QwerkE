68bbaf9eb14439cf229bfa2c94952318330a4d42
Aaron Appel <36603239+AaronAppel@users.noreply.github.com>
2024-04-06 09:19:39 -0400

    [GUID] Adding a simple GUID class to iterate with
    Example from Hazel engine by The Cherno

---
+++ Framework/Source/QC_Guid.h
@@ -0,0 +1,33 @@
+#pragma once
+
+// Sourced from : UUID.h -> https://github.com/TheCherno/Hazel/tree/master/Hazel/src/Hazel/Core
+
+namespace QwerkE {
+
+	class GUID
+	{
+	public:
+		GUID();
+		GUID(u64 GUID);
+		GUID(const GUID&) = default;
+
+		operator u64() const { return m_Guid; }
+	private:
+		u64 m_Guid;
+	};
+
+}
+
+namespace std {
+	template <typename T> struct hash;
+
+	template<>
+	struct hash<QwerkE::GUID>
+	{
+		std::size_t operator()(const QwerkE::GUID& guid) const
+		{
+			return (u64)guid;
+		}
+	};
+
+}
