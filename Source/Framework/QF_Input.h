87ff74642c1ad65de52651b1ac831ea6be7b5a49
Aaron Appel <36603239+AaronAppel@users.noreply.github.com>
2025-06-06 17:29:11 -0400

    [InputRefactor][Stage3Part0] Making some architecture decisions for new input API

--- Source/Framework/QF_Input.h
+++ Source/Framework/QF_Input.h
@@ -55,62 +55,18 @@ namespace QwerkE {
         void OnGamepad(const GamepadCallback& a_Callback);
         void OnGamepadStop(const GamepadCallback& a_Func);
 
+        GameActions& GetGameActions();
+
 #ifdef _QDEBUG
         void DrawDebugWindow();
 #endif // _QDEBUG
 
-        // Stage 3 //
-        // Devices and non-global or per-device input
-        // struct InputDevice
-        // {
-        //     int id;
-        //     int type; // 0 keyboard, 1 mouse, 2 joystick
-        //
-        //     bool enabled = true; // Disable for cinematics, UI input, window/UI loses focus
-        // };
-
-        // u8 DeviceCount();
-
-        // #NOTE Could be given to things like editor windows that can define a bool to know/call to enable/disable input (isFocused method or bool)
-        // InputDevice* Device(int deviceIdOrType) { return 0; } // #TODO Create device for user
-
-        // Stage 4 //
-        // More complex input tracking. Handle for users to be more full featured and convenient
-        // #NOTE Consuming input can mess with multi frame actions like holding, dragging, double click/press, repeating, counting, etc
-
-        // May need a time value as well like 0.25 seconds window for 2nd press. Can iterate over history buffer, but buffer will need timestamps added.
-        // bool KeyDoubled(int key, bool consume = false) { return false; } // For double click and double button press detection
-
-        // bool KeyRepeating(int key, bool consume = false) { return false; } // #TODO Look into Windows and GLFW key repeat behaviour
-
-        // keyHistorySize=150, keyHistory[keyHistorySize], keyHistoryTime[keyHistorySize]
-        // Look through key history buffer for key inputs over a_TimeSeconds. What if many inputs come in, or a_TimeSeconds is a large value?
-        // int KeyPressesInTime(const int a_Key, const float a_TimeSeconds) { return 0; }
-
-        // Time management. May be too niche and add overhead to input system that one or two user calls could manage themselves
-        // float KeyStateDuration(int key, bool consume = false) { return 0.f; }
-        // Versus
-        // float TimeSincePress(int key) { return 0.f; }
-        // float TimeSinceRelease(int key) { return 0.f; }
-
-        // Stage 5 //
-        // Input mappings and complex input actions
-        // struct InputAction
-        // {
-        //     int id;
-        //     char keys[5];
-        // };
-
-        GameActions& GetGameActions();
         // bool ActionPressed(const InputAction a_Action);
         // bool ActionReleased(const InputAction a_Action);
         // bool ActionDown(const InputAction a_Action);
 
-        // Maybe fun to have but really just extra. Behaves like registering and unregistering plus counting.
-        // Really just to avoid user from managing state, but at what cost?
-        // void StartPressCounter(int key) { }
-        // u16 GetPressCounter(int key) { return 0; }
-        // void StopPressCounter(int key) { }
+        // Stage 3 //
+        // Clean up standard API, and look at adding input mapping and contextual input handling
 
         // Additional considerations //
         // Input layering, consumption, or otherwise avoiding/ignoring input based on context such as focused windows and gameplay state(s)
@@ -129,7 +85,17 @@ namespace QwerkE {
         // m_Handler->SetContexts(handlerContexts);
         // m_Handler->RegisterEventCallbacks(true); // #NOTE To automatically disable on game events like game paused, using assigned contexts
         // m_Handler->SetEnabled(true);
+        // if (m_Handler->Activated("Jump")) { player.Jump(); }
+        // if (m_Handler->IsActivate("Walk")) { player.pos += player.forward * player.walkSpeed; }
+        // if (m_Handler->Deactivate("Move")) { player.isSprinting = false; }
 
+        // Maybe fun to have but really just extra. Behaves like registering and unregistering plus counting.
+        // Really just to avoid user from managing state, but at what cost?
+        // void Input::InputHandlerStartPressCounter(int key) { }
+        // u16 Input::InputHandlerGetPressCounter(int key) { return 0; }
+        // void Input::InputHandlerStopPressCounter(int key) { }
+
+        // InputDevice object as handler?
     }
 
 }
