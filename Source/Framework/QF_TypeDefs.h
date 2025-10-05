acdb5a36b4575f912f0877fba107cb9f3406be2b
Aaron Appel <36603239+AaronAppel@users.noreply.github.com>
2025-10-05 08:29:54 -0400

    [MyTypes] Adding custom math type Vector4f

--- Source/Framework/QF_TypeDefs.h
+++ Source/Framework/QF_TypeDefs.h
@@ -164,14 +164,14 @@ const float mat4Identity[16] = {
 
 #endif
 
-// #include "QC_Vector2.h"
+// #include "QF_Vector2.h"
 // typedef QwerkE::Vector2f vec2f;
 // typedef QwerkE::Vector2u16 vec2u16;
 
-// #include "QC_Vector3.h"
+// #include "QF_Vector3.h"
 // typedef QwerkE::Vector3f vec3f;
 
-// #include "QC_Vector4.h"
+#include "QF_Vector4.h"
 // typedef QwerkE::Vector4f vec4f;
 
 #include "QF_Matrix4.h"
