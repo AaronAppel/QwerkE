0f537b7356e341ad2b8c7dd87f311cf943aa5c97
Aaron Appel <36603239+AaronAppel@users.noreply.github.com>
2026-03-29 16:36:55 -0700

    [Prefab] Adding a note for a future feature
    Eventually I will want a re-usable entity type that can be referenced infinite times, but also overridden where applicable. It would also be cool to look at scenes referencing each other (in full or part) to save duplication and manual updating (propagation) while still allowing variations within each scene. Could be useful for cases like client and server scenes inheriting some but not all functionality, from each other or some base/common/parent scene.

---
+++ Source/Editor/QE_Prefab.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace QwerkE {
+
+    // #TODO Design and implement prefabs.
+    // Editor only. Allow support for scenes to reference each other, or create some SceneRef with Scene specific support.
+    // A way of representing a re-usable entity structure of any kind.
+    // Generic enough to be used in a game scene, prefab specific scene, and possibly as a SceneRef.
+
+}
